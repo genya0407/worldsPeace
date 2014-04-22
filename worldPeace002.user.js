d7bfc3e5276e1e2c5a6542ebc7e767bbd1fd14c5
Y.Sangenya <longinus.eva@gmail.com>
2014-04-22 19:11:31 +0900

    第二回

---
+++ worldPeace002.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name    KMC_JS_001_Background_Changer
+// @include *
+// ==/UserScript==
+var url = 'http://continue.c.blog.so-net.ne.jp/_images/blog/_991/continue/nadeko11-326e1.jpg?c=a312';
+document.body.style.background = 'url("' + url + '")';
