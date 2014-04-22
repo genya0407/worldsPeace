fc9402cf30d7bef8bd0510eb8da67224f561dac3
Y.Sangenya <longinus.eva@gmail.com>
2014-04-22 20:44:13 +0900

    switch modified

--- worldPeace002.user.js
+++ worldPeace002.user.js
@@ -8,30 +8,23 @@ function changeBackground(url){
     document.body.style.background = 'url("' + url + '")';
 }
 
+function changeLevel(url){
+    return url.split(".").slice(1).join(".");
+}
 
+var highLevelUrl = changeLevel(location.host);
 
-var url = location.host;
-
-/*
-switch(url){
-    case "www.google.co.jp":
+switch(highLevelUrl){
+    case "google.co.jp":
     changeBackground("https://lh4.googleusercontent.com/-LiNMdlmi2PE/UwGTCMEip8I/AAAAAAAAABA/dEsGNSfk2fE/social-google-box-icon.jpg");
     break;
 
-    case "www.yahoo.co.jp":
+    case "yahoo.co.jp":
+    case "search.yahoo.co.jp":
     changeBackground("http://images.bitsaa.org/News/2008/08/25aug200817.jpg");
     break;
 
     default:
     console.log("hoge");
     break;
-}
-*/
-
-if(url === "www.google.co.jp"){
-    changeBackground("https://lh4.googleusercontent.com/-LiNMdlmi2PE/UwGTCMEip8I/AAAAAAAAABA/dEsGNSfk2fE/social-google-box-icon.jpg");
-}else if((/yahoo/).test(url)){
-    changeBackground("http://images.bitsaa.org/News/2008/08/25aug200817.jpg");
-}else{
-    console.log("Where is this?");
 }
