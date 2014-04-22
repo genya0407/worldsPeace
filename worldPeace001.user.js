0fbbe2e5cf67043b7a336a3f2cdd83c3fc6b6050
U-yusuke-note\yusuke <yusuke@yusuke-note.(none)>
2014-04-22 19:08:32 +0900

    第一回

---
+++ worldPeace001.user.js
@@ -0,0 +1,8 @@
+﻿// ==UserScript==
+// @name	backgound_hitagi
+// @include *
+// ==/UserScript==
+
+var urlToHitagi = "http://kabegamisouko.net/bakemonogatari/hitagi/19201080/0048.jpg";
+document.body.style.background = "url('" + urlToHitagi + "')";
+
